5cf4f86f4ae13fb497f6be72454174ebd4e8cd75
DEsimas <simasuh@gmail.com>
2022-11-01 19:52:41 +0300

    init position

--- src/components/Transpose.js
+++ src/components/Transpose.js
@@ -3,11 +3,11 @@ import { getZeroMatrix, transpose } from '../Matrix'
 import Input from './Input'
 import './Transpose.css'
 
-const Transpose = () => {
+const Transpose = (props) => {
     const [matrix1, setMatrix1] = useState(getZeroMatrix(3, 3))
     const [matrix2, setMatrix2] = useState(getZeroMatrix(3, 3))
     const [isMoving, setIsMoving] = useState(false)
-    const [position, setPosition] = useState({ top: 0, left: 400 })
+    const [position, setPosition] = useState({ top: props.top ?? 0, left: props.left ?? 0 })
 
     return (
         <div onMouseDown={(e) => { setIsMoving(true) }} onMouseUp={() => { setIsMoving(false) }}
