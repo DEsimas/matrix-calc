1d41a8fd3b74d7e04b7fc58bcfb8751a1e9994f6
DEsimas <simasuh@gmail.com>
2022-11-01 19:05:04 +0300

    Transpose styles

--- src/components/Transpose.js
+++ src/components/Transpose.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { getZeroMatrix, transpose } from '../Matrix'
 import Input from './Input'
+import './Transpose.css'
 
 const Transpose = () => {
     const [matrix1, setMatrix1] = useState(getZeroMatrix(3, 3))
@@ -8,17 +9,19 @@ const Transpose = () => {
 
     return (
         <div className='transpose'>
-            <h2>Transpose</h2>
-            <div className='transpose-input'>
-                <Input value={matrix1} onChange={(matrix) => {
-                    setMatrix2(transpose(matrix))
+            <h2 className='transpose-header'>Transpose</h2>
+            <div className='transpose-desktop'>
+                <div className='transpose-input'>
+                    <Input value={matrix1} onChange={(matrix) => {
+                        setMatrix2(transpose(matrix))
+                    }} />
+                    <span className='transpose-span'>T</span>
+                </div>
+                <span>=</span>
+                <Input value={matrix2} onChange={(matrix) => {
+                    setMatrix1(transpose(matrix))
                 }} />
             </div>
-            <span>T</span>
-            <span>=</span>
-            <Input value={matrix2} onChange={(matrix) => {
-                setMatrix1(transpose(matrix))
-            }} />
         </div>
     )
 }
