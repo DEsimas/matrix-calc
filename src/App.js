6cd582ade759837197edc931b5bc0a158bc8b752
DEsimas <simasuh@gmail.com>
2022-11-02 15:24:01 +0300

    make app component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from 'react'
-import Transpose from './components/Transpose'
+import React, { useState } from 'react'
 import './normalize.css'
 import './App.css'
 import Table from './components/Table'
 
 const App = () => {
+    const [elements, setElements] = useState([])
+
+    const addElement = (element) => {
+        elements.push(element)
+        setElements([...elements])
+    }
+
     return (
         <>
-            <Table />
+            <Table addElement={addElement} />
+            <div className='elements'>
+                {elements.map(el => (el))}
+            </div>
         </>
     )
 }
