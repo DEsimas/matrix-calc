320eaad3ca3e52bf5996ec8c43b32dd4d5f1eecc
DEsimas <simasuh@gmail.com>
2022-11-02 14:45:15 +0300

    on close fix

--- src/components/Transpose.js
+++ src/components/Transpose.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
+import { v1 } from 'uuid'
 import { getZeroMatrix, transpose } from '../Matrix'
 import Input from './Input'
 import './Transpose.css'
@@ -6,12 +7,13 @@ import './Transpose.css'
 const Transpose = (props) => {
     const [matrix1, setMatrix1] = useState(getZeroMatrix(3, 3))
     const [matrix2, setMatrix2] = useState(getZeroMatrix(3, 3))
+    const id = v1()
 
     return (
-        <div className='transpose movable'>
+        <div id={id} className='transpose movable'>
             <div className='transpose-top'>
                 <h2 className='transpose-header'>Transpose</h2>
-                <button onClick={() => { typeof props.onClose == 'function' && props.onClose() }} className='transpose-exit'>X</button>
+                <button onClick={() => { document.getElementById(id).classList.add('hidden') }} className='transpose-exit'>X</button>
             </div>
             <div className='transpose-desktop'>
                 <div className='transpose-input'>
