73674f387eb8fc1f10feb53313a7a5546af7e6da
DEsimas <simasuh@gmail.com>
2022-11-05 15:06:12 +0300

    addZeroColumn tests

--- src/matrix/resize/addZeroColumn.js
+++ src/matrix/resize/addZeroColumn.js
@@ -1,4 +1,7 @@
+import { isMatrix } from "./../validation/isMatrix"
+
 export function addZeroColumn(matrix) {
+    if (!isMatrix(matrix)) throw Error('Matrix have to be passed')
     for (let i = 0; i < matrix.length; i++) {
         matrix[i].push(0)
     }
