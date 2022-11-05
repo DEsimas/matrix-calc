05c2bc3d59177a85723fafea6a1b635303578399
DEsimas <simasuh@gmail.com>
2022-11-05 14:18:50 +0300

    fix getZeroMatrix

--- src/matrix/generate/getZeroMatrix.js
+++ src/matrix/generate/getZeroMatrix.js
@@ -1,9 +1,12 @@
 import { getZeroVector } from "./getZeroVector"
 import { isNumber } from "./../validation/isNumber"
+import { isPositiveInteger } from "./../validation/isPositiveInteger"
 
-export function getZeroMatrix(width, height) {
-    if (!isNumber(width)) throw Error(`Size have to be a number, recieved ${width}`)
-    if (!isNumber(height)) throw Error(`Size have to be a number, recieved ${height}`)
+export function getZeroMatrix(height, width) {
+    if (!isNumber(height)) throw Error(`Height have to be a number, recieved ${height}`)
+    if (!isPositiveInteger(height)) throw Error(`Height have to be positive integer, recieved ${height}`)
+    if (!isNumber(width)) throw Error(`Width have to be a number, recieved ${width}`)
+    if (!isPositiveInteger(width)) throw Error(`Width have to be positive integer, recieved ${width}`)
     const matrix = []
     for (let i = 0; i < height; i++) {
         matrix.push(getZeroVector(width))
