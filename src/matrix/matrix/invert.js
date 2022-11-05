f246fa2afb7dd91dc74d59a137c28952567ede96
DEsimas <simasuh@gmail.com>
2022-11-05 21:46:39 +0300

    fix invert

--- src/matrix/matrix/invert.js
+++ src/matrix/matrix/invert.js
@@ -1,7 +1,9 @@
 import { adjugate } from "./adjugate"
 import { determinant } from "./determinant"
+import { isMatrix } from "../validation/isMatrix"
 import { divideByNumber } from "./../matrixByNumber/divideByNumber"
 
 export function invert(matrix) {
+    if (!isMatrix(m)) throw Error('Matrix have to be passed')
     return divideByNumber(adjugate(matrix), determinant(matrix));
 }
