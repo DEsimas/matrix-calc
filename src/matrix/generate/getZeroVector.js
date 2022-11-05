30c2688c259e24aad2add99374e334a26d11296d
DEsimas <simasuh@gmail.com>
2022-11-05 14:25:00 +0300

    getZeroVector fix

--- src/matrix/generate/getZeroVector.js
+++ src/matrix/generate/getZeroVector.js
@@ -1,4 +1,9 @@
+import { isNumber } from "./../validation/isNumber"
+import { isPositiveInteger } from "./../validation/isPositiveInteger"
+
 export function getZeroVector(length) {
+    if (!isNumber(length)) throw Error(`Length have to be a number, recieved ${length}`)
+    if (!isPositiveInteger(length)) throw Error(`Length have to be positive integer, recieved ${length}`)
     const vec = []
     for (let i = 0; i < length; i++)
         vec.push(0)
