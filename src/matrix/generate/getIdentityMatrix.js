f7a87f1de12caf19ec8ceb582c65f60dec070a73
DEsimas <simasuh@gmail.com>
2022-11-05 14:06:15 +0300

    fix getIdentityMatrix

--- src/matrix/generate/getIdentityMatrix.js
+++ src/matrix/generate/getIdentityMatrix.js
@@ -1,9 +1,13 @@
 import { getZeroMatrix } from "./getZeroMatrix"
+import { isNumber } from "./../validation/isNumber"
+import { isPositiveInteger } from "./../validation/isPositiveInteger"
 
 export function getIdentityMatrix(size) {
+    if (!isNumber(size)) throw Error(`Size have to be a number, recieved ${size}`)
+    if (!isPositiveInteger(size)) throw Error(`Size have to be positive integer, recieved ${size}`)
     const matrix = getZeroMatrix(size, size)
     for (let i = 0; i < size; i++) {
-        matrix[size][size] = 1
+        matrix[i][i] = 1
     }
     return matrix
 }
--- src/matrix/generate/getZeroMatrix.js
+++ src/matrix/generate/getZeroMatrix.js
@@ -1,6 +1,9 @@
 import { getZeroVector } from "./getZeroVector"
+import { isNumber } from "./../validation/isNumber"
 
 export function getZeroMatrix(width, height) {
+    if (!isNumber(width)) throw Error(`Size have to be a number, recieved ${width}`)
+    if (!isNumber(height)) throw Error(`Size have to be a number, recieved ${height}`)
     const matrix = []
     for (let i = 0; i < height; i++) {
         matrix.push(getZeroVector(width))
