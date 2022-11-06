34224a1977541ed22f121ba75973bc74309cadaa
DEsimas <simasuh@gmail.com>
2022-11-06 12:13:59 +0300

    replace button

--- src/components/windows/DoubleMatrix.js
+++ src/components/windows/DoubleMatrix.js
@@ -26,7 +26,18 @@ const DoubleMatrix = (props) => {
                     value={matrix1}
                     onChange={(matrix) => setMatrix1(matrix)}
                 />
-                <span className='double_matrix-span'>{props.span}</span>
+                <div className='double_matrix-mid'>
+                    <span className='double_matrix-span'>{props.span}</span>
+                    <svg
+                        onClick={() => {
+                            const t = matrix1
+                            setMatrix1(matrix2)
+                            setMatrix2(t)
+                        }}
+                        xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-arrow-left-right" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M1 11.5a.5.5 0 0 0 .5.5h11.793l-3.147 3.146a.5.5 0 0 0 .708.708l4-4a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 11H1.5a.5.5 0 0 0-.5.5zm14-7a.5.5 0 0 1-.5.5H2.707l3.147 3.146a.5.5 0 1 1-.708.708l-4-4a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 4H14.5a.5.5 0 0 1 .5.5z" />
+                    </svg>
+                </div>
                 <Input
                     value={matrix2}
                     onChange={(matrix) => setMatrix2(matrix)}
